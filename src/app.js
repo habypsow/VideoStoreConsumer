b5b99cf3d7a126185122479be16e7e6d41616e74
Haby Randall <sow.haby@gmail.com>
2017-06-14 13:22:46 -0700

    Added movielist model to app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,18 @@
 // Import jQuery & Underscore
 import $ from 'jquery';
 import _ from 'underscore';
+import MovieList from './collections/movie_list';
+import MovieListView from './views/movie_list_view';
 
+var rentalLibraryList = new MovieList({});
+
+var rentalLibraryListView = new MovieListView({
+  model: rentalLibraryList,
+  template: _.template($('#rental-card-template').html()),
+  el: 'main'
+});
 // ready to go
 $(document).ready(function() {
 
-  $('section.main-content').append('<p>Hello World!</p>');
-
+rentalLibraryListView.render();
 });
